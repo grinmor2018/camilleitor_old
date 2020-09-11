fbfb774849b2f4c2e4ab112d2dd80652e43b1f01
David Marquez <grinmor2016@gmail.com>
2020-09-11 22:52:41 +0200

    primer commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Frontend
+# Camillatormean
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import {  HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +15,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule
