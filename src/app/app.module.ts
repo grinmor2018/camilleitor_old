bb4020a09e48e31034e0d7d6e3b41950abd45c8a
David Marquez <grinmor2016@gmail.com>
2020-09-14 19:08:31 +0200

    interfaz horizontal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TaskDetailsComponent } from './tasks/task-details/task-details.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent,
-    TaskDetailsComponent,
-    TaskListComponent
+    TasksComponent
   ],
   imports: [
     BrowserModule,
